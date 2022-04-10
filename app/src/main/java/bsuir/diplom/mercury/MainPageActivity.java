87ea8d2a4a70a2ec77364d3fcecea07ede8d1d34
Nikita Menshikov <menshikov.mikita@gmail.com>
2022-04-10 20:19:00 +0300

    Implemented bottom sheet dialof with CoordinatorLayout

--- app/src/main/java/bsuir/diplom/mercury/MainPageActivity.java
+++ app/src/main/java/bsuir/diplom/mercury/MainPageActivity.java
@@ -1,11 +1,8 @@
 package bsuir.diplom.mercury;
 
 import android.os.Bundle;
-import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 public class MainPageActivity extends AppCompatActivity {
 
@@ -13,15 +10,5 @@ public class MainPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.main_frame);
-        if (fragment == null) {
-            fragment = new MainPageFragment();
-            fm.beginTransaction()
-                    .add(R.id.main_frame, fragment)
-                    .commit();
-        }
     }
 }
