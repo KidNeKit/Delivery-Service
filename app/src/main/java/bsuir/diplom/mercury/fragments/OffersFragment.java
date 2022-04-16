4e06954568b215ba2dd0cda5733a1cc4dcf4170d
Nikita Menshikov <menshikov.mikita@gmail.com>
2022-04-16 19:43:15 +0300

    Implemented view pager for main page

---
+++ app/src/main/java/bsuir/diplom/mercury/fragments/OffersFragment.java
@@ -0,0 +1,21 @@
+package bsuir.diplom.mercury.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import bsuir.diplom.mercury.R;
+
+public class OffersFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_offers, container, false);
+    }
+}
