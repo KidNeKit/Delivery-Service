2ab4f08a6a8181b32c0de3b674fd8ca182160fcd
Nikita Menshikov <menshikov.mikita@gmail.com>
2022-05-09 17:42:28 +0300

    First presentation of PersonalChoosingFragment

--- app/src/main/java/bsuir/diplom/mercury/fragments/offerCreation/PersonalChoosingFragment.java
+++ app/src/main/java/bsuir/diplom/mercury/fragments/offerCreation/PersonalChoosingFragment.java
@@ -5,6 +5,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -13,11 +17,14 @@ import bsuir.diplom.mercury.R;
 import bsuir.diplom.mercury.interfaces.ViewPagerFragmentLifecycle;
 
 public class PersonalChoosingFragment extends Fragment implements ViewPagerFragmentLifecycle {
-    private static ChangeAddedItemFragment instance;
+    private static PersonalChoosingFragment instance;
+    private RadioGroup carSelectionRadioGroup;
+    private RadioButton mediumWeightCarButton;
+    private RadioButton lightWeightCarButton;
 
-    public static ChangeAddedItemFragment getInstance() {
+    public static PersonalChoosingFragment getInstance() {
         if (instance == null) {
-            instance = new ChangeAddedItemFragment();
+            instance = new PersonalChoosingFragment();
         }
         return instance;
     }
@@ -27,16 +34,54 @@ public class PersonalChoosingFragment extends Fragment implements ViewPagerFragm
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_personal_choosing, container, false);
 
+        carSelectionRadioGroup = view.findViewById(R.id.car_selection_radio_group);
+        mediumWeightCarButton = view.findViewById(R.id.medium_weight_car_button);
+        lightWeightCarButton = view.findViewById(R.id.light_weight_car_button);
+
+        mediumWeightCarButton.setOnClickListener(onCarSelectedListener);
+        lightWeightCarButton.setOnClickListener(onCarSelectedListener);
+
+        SeekBar loaderSeekBar = view.findViewById(R.id.loader_seek_bar);
+
+        loaderSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
         return view;
     }
 
     @Override
     public void onPauseFragment(FragmentManager parentFragmentManager) {
-        Log.d("lifecycle", "onPause for ChangeAddedItemFragment");
+        Log.d("lifecycle", "onPause for PersonalChoosingFragment");
     }
 
     @Override
     public void onResumeFragment(FragmentManager parentFragmentManager) {
-        Log.d("lifecycle", "onResumeFragment for ChangeAddedItemFragment");
+        Log.d("lifecycle", "onResumeFragment for PersonalChoosingFragment");
     }
+
+    private final View.OnClickListener onCarSelectedListener = view -> {
+        switch (carSelectionRadioGroup.getCheckedRadioButtonId()) {
+            case R.id.medium_weight_car_button:
+                Toast.makeText(getContext(), "Medium weight", Toast.LENGTH_SHORT);
+                break;
+            case R.id.light_weight_car_button:
+                Toast.makeText(getContext(), "Light weight", Toast.LENGTH_SHORT);
+                break;
+        }
+    };
 }
