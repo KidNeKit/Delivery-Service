11d496d337c62341ba57dc1c31fc1755a05fe5e2
Nikita Menshikov <menshikov.mikita@gmail.com>
2022-05-17 19:01:51 +0300

    Added onClickListener for views

--- app/src/main/java/bsuir/diplom/mercury/fragments/offerCreation/DestinationPointsFragment.java
+++ app/src/main/java/bsuir/diplom/mercury/fragments/offerCreation/DestinationPointsFragment.java
@@ -1,14 +1,18 @@
 package bsuir.diplom.mercury.fragments.offerCreation;
 
+import android.location.Address;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+
+import com.yandex.mapkit.geometry.Point;
 
 import java.util.ArrayList;
 
@@ -19,6 +23,8 @@ import bsuir.diplom.mercury.utils.Constants;
 
 public class DestinationPointsFragment extends Fragment implements ViewPagerFragmentLifecycle {
     private static DestinationPointsFragment instance;
+    private Point fromPoint;
+    private Point toPoint;
 
     public static DestinationPointsFragment getInstance() {
         if (instance == null) {
@@ -34,10 +40,28 @@ public class DestinationPointsFragment extends Fragment implements ViewPagerFrag
 
         AutoCompleteTextView fromEditText = view.findViewById(R.id.from_edit_text);
         AutoCompleteTextView toEditText = view.findViewById(R.id.to_edit_text);
+        Button createOfferButton = view.findViewById(R.id.create_offer);
 
         AddressSuggestAdapter addressSuggestAdapter = new AddressSuggestAdapter(getContext(), R.layout.single_address_suggest, new ArrayList<>());
         fromEditText.setAdapter(addressSuggestAdapter);
         toEditText.setAdapter(addressSuggestAdapter);
+
+        fromEditText.setOnItemClickListener((adapterView, view1, i, l) -> {
+            Address address = (Address) adapterView.getItemAtPosition(i);
+            fromPoint = new Point(address.getLatitude(), address.getLongitude());
+        });
+
+        toEditText.setOnItemClickListener((adapterView, view1, i, l) -> {
+            Address address = (Address) adapterView.getItemAtPosition(i);
+            toPoint = new Point(address.getLatitude(), address.getLongitude());
+        });
+
+        createOfferButton.setOnClickListener(view1 -> {
+            if (fromPoint == null || toPoint == null) {
+                return;
+            }
+            Log.d("Create Offer", "offer creation");
+        });
 
         return view;
     }
